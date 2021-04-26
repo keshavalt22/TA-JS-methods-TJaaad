9365b8db8d5eeac6094bc1d8bf23c4bbd9eb4a91
Keshav Khatri <keshavkhatri35@gmail.com>
2021-04-26 12:18:46 +0530

    assignment

--- block-BJaacz/code/index.js
+++ block-BJaacz/code/index.js
@@ -3,36 +3,69 @@
 /*
   Create a function named addTwo which accepts a number, adds 2 to the number and return the new value.
 */
-
+function addTwo(num) {
+  return num + 2;
+}
 /*
   Create a function named addThree which accepts a number, adds 3 to the number and return the new value.
 */
-
+function addThree(num) {
+  return num + 3;
+}
 /*
   Create a function named addFive which accepts a number, adds 5 to the number and return the new value.
 */
-
+function addFive(num) {
+  return num + 5;
+}
 /*
   Create a function named addTwoToArray which accepts:
     - An array of numbers
     - Returns a new array where 2 is added to each element
     - While doing so use the funciton addTwo
 */
+function addTwoToArray(arr){
+  let finalArray = [];
 
+  for(let num of arr) {
+      let a = addTwo(num);{
+        addTwoToArray.push;
+      }
+  }
+  return finalArray;
+}
+
+console.log(addTwoToArray([1, 2, 3, 4, 5, 6]));
 /*
   Create a function named addThreeToArray which accepts:
     - an array of numbers
-    - aeturns a new array where 2 is added to each element
+    - aeturns a new array where 3 is added to each element
     - while doing so use the funciton addThree
 */
+function addThreeToArray(arr){
+  let finalArray = [];
 
+  for(let num of arr) {
+      let a = addThree(num);
+      finalArray.push(num);
+  }
+  return finalArray;
+}
 /*
   Create a function named addFiveToArray which accepts:
     - an array of numbers
-    - aeturns a new array where 2 is added to each element
+    - aeturns a new array where 5 is added to each element
     - while doing so use the funciton addThree
 */
+function addFiveToArray(arr){
+  let finalArray = [];
 
+  for(let num of arr) {
+      let a = addFive(num);
+      finalArray.push(num);
+  }
+  return finalArray;
+}
 /*
 In above function addTwoToArray, addThreeToArray, addFiveToArray we are repeating the code, let's fix this.
 
