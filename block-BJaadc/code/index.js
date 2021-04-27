ce8738defcdfeeea34251977a4de5cbb73cf2b36
Keshav Khatri <keshavkhatri35@gmail.com>
2021-04-27 14:09:26 +0530

    assignment string methods

--- block-BJaadc/code/index.js
+++ block-BJaadc/code/index.js
@@ -2,15 +2,27 @@ let message = 'If you are still thinking the answer is no!';
 
 // Log the length of the variable message like `The length of [message] is [length]`
 
+console.log(`The length of [message] is ${message.length}.`);
+
 // Log the character at index 0 of the variable message like `The first character of [message] is [character]`
+
+console.log(`The first character of [message] is ${message.indexOf(`I`)}.`);
 
 // Log the last character of message using length like `The last character of [message] is [character]`
 
+console.log(`The last character of [message] is `);
+
 // Log the index of word "you" in message
+
+console.log(message.indexOf(`you`));
 
 // Log the index of word "still" in message
 
-// Log the index of word "answers" in message
+console.log(message.indexOf(`still`));
+
+// Log the index of word "answer" in message
+
+console.log(message.indexOf(`answer`));
 
 // Log true or false based on whether the word "answers" exist in message or not
 
