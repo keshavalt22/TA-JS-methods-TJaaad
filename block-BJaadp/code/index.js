e129fc8f104317b43ce47577a2c46aadfe70de13
Keshav Khatri <keshavkhatri35@gmail.com>
2021-05-20 12:10:29 +0530

    assignment 3

--- block-BJaadp/code/index.js
+++ block-BJaadp/code/index.js
@@ -81,7 +81,14 @@ Output:
 {banana: 2, cherry: 3, orange: 3, apple: 2, fig: 1}
 */
 
-fruitBasket.filter
+let fruitsObj = fruitBasket.reduce((acc, cv) => {
+  if(acc[cv]){
+    acc[cv] = acc[cv] + 1;  
+  }else{
+    acc[cv] = 1;
+  }
+  return acc;
+},{});
 
 /* 
 
@@ -93,6 +100,11 @@ Output:
 [['banana', 2], ['cherry', 3], ['orange', 3], ['apple', 2], ['fig', 1]]
 */
 
+let keys = Object.keys(fruitsObj).reduce((acc,cv) => {
+  acc = acc.concat([[cv,fruitsObj[cv]]]);
+  return acc;
+},[]);
+
 const data = [
   [1, 2, 3],
   [4, 5, 6],
@@ -100,7 +112,13 @@ const data = [
   [10, 11, 12],
 ];
 
+
 // Using reduce flat data array
+
+data.reduce((acc,cv) => {
+  acc = acc.concat(cv);
+  return acc;
+},[]);
 
 const dataTwo = [
   [1, 2, 3],
@@ -110,6 +128,11 @@ const dataTwo = [
 ];
 
 // Using reduce flat dataTwo array
+
+dataTwo.reduce((acc,cv) => {
+  acc = acc.concat(cv.flat(Infinity));
+  return acc;
+},[]);
 
 /*
 
@@ -121,16 +144,36 @@ Create these functions which accepts a number value and returns a number value:
   - `half` converts the value to half and return the integer value not decimal (use Math.round(21.5) => 21)
 */
 
-// let pipeline = [
-//   increment,
-//   double,
-//   decrement,
-//   decrement,
-//   double,
-//   triple,
-//   half,
-//   increment,
-// ];
+function increment(num){
+  return num + 1;
+}
+
+function double(num){
+  return num * 2;
+}
+
+function decrement(num){
+  return num - 1;
+}
+
+function triple(num){
+  return num * 3;
+}
+
+function half(num){
+  return Math.round(num / 2);
+}
+
+let pipeline = [
+  increment,
+  double,
+  decrement,
+  decrement,
+  double,
+  triple,
+  half,
+  increment,
+];
 
 /*
 Using the pipeline variable that contains the collection of functions, taking the initial value 3 find the output.
@@ -146,18 +189,28 @@ EXAMPLE:
   ...
 */
 
-// let pipeline2 = [
-//   increment,
-//   half,
-//   double,
-//   decrement,
-//   decrement,
-//   triple,
-//   double,
-//   triple,
-//   half,
-//   increment,
-//   triple,
-// ];
+pipeline.reduce((acc,cv) => {
+  acc = cv(acc);
+  return acc;
+},3)
+
+let pipeline2 = [
+  increment,
+  half,
+  double,
+  decrement,
+  decrement,
+  triple,
+  double,
+  triple,
+  half,
+  increment,
+  triple,
+];
 
 // Find the output using pipeline2 the initial value if 8
+
+pipeline2.reduce((acc,cv) => {
+  acc = cv(acc);
+  return acc;
+},8);
